55c066c1099a9cc7f0a9fd660a7e94dcfb3db2e1
ClayDreY <107676023+ClayDreY@users.noreply.github.com>
2022-10-03 22:45:53 +0100

    Update comingsoon.php
    Se sustituye <link rel="stylesheet" por <link href="stylesheet"  Línea 17

--- www/wp-content/plugins/iu-proximamente/interface/comingsoon.php
+++ www/wp-content/plugins/iu-proximamente/interface/comingsoon.php
@@ -14,7 +14,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="stylesheet" href="https://fonts.googleapis.com/css?family=Baloo+Paaji+2&display=swap" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Baloo+Paaji+2&display=swap" >
     <link rel="stylesheet" href="<?php echo plugins_url('assets/css/comingsoon.css', dirname(__FILE__)) ?>">
     <script src="<?php echo plugins_url('assets/js/comingsoon.js', dirname(__FILE__)) ?>" type="text/javascript"></script>
     <title>Próximamente | <?php bloginfo('name') ?></title>
